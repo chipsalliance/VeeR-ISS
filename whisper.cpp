b993d1931bee541696214cd1469decd6ca16a023
Joseph Rahmeh <joseph.rahmeh@wdc.com>
2020-02-27 11:13:40 -0800

    Added support for breaking long running target program under gbd.

--- whisper.cpp
+++ whisper.cpp
@@ -240,7 +240,7 @@ void
 printVersion()
 {
   unsigned version = 1;
-  unsigned subversion = 476;
+  unsigned subversion = 477;
   std::cout << "Version " << version << "." << subversion << " compiled on "
 	    << __DATE__ << " at " << __TIME__ << '\n';
 }
