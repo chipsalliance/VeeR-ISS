95aea7bb461ba9b0226a71d0c5bda20fc4747e6f
Joseph Rahmeh <joseph.rahmeh@wdc.com>
2020-08-24 07:02:48 -0700

    Reading CSR SATP now triggers an illegal instruction exception if in supervisor mode and MSTATUS.TVM is 1.

--- whisper.cpp
+++ whisper.cpp
@@ -246,7 +246,7 @@ void
 printVersion()
 {
   unsigned version = 1;
-  unsigned subversion = 564;
+  unsigned subversion = 565;
   std::cout << "Version " << version << "." << subversion << " compiled on "
 	    << __DATE__ << " at " << __TIME__ << '\n';
 }
