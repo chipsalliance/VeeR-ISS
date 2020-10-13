7d48c26910088a5b420ef1eaf3a24f06657e4867
Joseph Rahmeh <joseph.rahmeh@wdc.com>
2020-10-12 17:57:39 -0700

    Vector bringup: Debugged wide integer multiply.

--- wideint.cpp
+++ wideint.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <iostream>
 #include "wideint.hpp"
 
 using namespace WdRiscv;
@@ -7,28 +8,36 @@ using namespace WdRiscv;
 Uint128&
 Uint128::operator *= (const Uint128& x)
 {
-  QuarterType* multiplicand = (QuarterType*) &low_;
-  QuarterType* multiplier = (QuarterType*) &x.low_;
+  unsigned qw = width() / 4;
 
-  SelfType prod[2];
-  QuarterType* acc = (QuarterType*) &prod[0];
+  QuarterType a0 = (low_ << qw) >> qw, a1 = low_ >> qw;
+  QuarterType a2 = (high_ << qw) >> qw, a3 = high_ >> qw;
+  QuarterType b0 = (x.low_ << qw) >> qw, b1 = x.low_ >> qw;
+  QuarterType b2 = (x.high_ << qw) >> qw, b3 = x.high_ >> qw;
 
-  for (unsigned i = 0; i < 4; ++i, ++acc)
-    {
-      QuarterType a = multiplier[i];
-      QuarterType carry = 0;
-      QuarterType* row = acc;
-      for (unsigned j = 0; j < 4; ++j, ++row)
-        {
-          HalfType b = multiplicand[j];
-          b *= a;
-          HalfType* sum = (HalfType*) row;
-          HalfType prev = *sum;
-          *sum += b + HalfType(carry);
-          carry = (*sum < prev)? QuarterType(1) : QuarterType(0);
-        }
-    }
-  *this = prod[0];
+  *this = 0;
+  SelfType shifted = 0;
+
+  HalfType prod = a0; prod *= b0; *this += prod;
+  prod = a0; prod *= b1; shifted = prod; shifted <<= qw; *this += shifted;
+  prod = a0; prod *= b2; shifted = prod; shifted <<= 2*qw; *this += shifted;
+  prod = a0; prod *= b3; shifted = prod; shifted <<= 3*qw; *this += shifted;
+
+  prod = a1; prod *= b0; shifted = prod; shifted <<= qw; *this += shifted;
+  prod = a1; prod *= b1; shifted = prod; shifted <<= 2*qw; *this += shifted;
+  prod = a1; prod *= b2; shifted = prod; shifted <<= 3*qw; *this += shifted;
+  prod = a1; prod *= b3; shifted = prod; shifted <<= 4*qw; *this += shifted;
+
+  prod = a2; prod *= b0; shifted = prod; shifted <<= 2*qw; *this += shifted;
+  prod = a2; prod *= b1; shifted = prod; shifted <<= 3*qw; *this += shifted;
+  prod = a2; prod *= b2; shifted = prod; shifted <<= 4*qw; *this += shifted;
+  prod = a2; prod *= b3; shifted = prod; shifted <<= 5*qw; *this += shifted;
+
+  prod = a3; prod *= b0; shifted = prod; shifted <<= 3*qw; *this += shifted;
+  prod = a3; prod *= b1; shifted = prod; shifted <<= 4*qw; *this += shifted;
+  prod = a3; prod *= b2; shifted = prod; shifted <<= 5*qw; *this += shifted;
+  prod = a3; prod *= b3; shifted = prod; shifted <<= 6*qw; *this += shifted;
+
   return *this;
 }
 
@@ -356,7 +365,7 @@ Uint256::operator *= (const Uint256& x)
   for (unsigned i = 0; i < 4; ++i, ++acc)
     {
       QuarterType a = multiplier[i];
-      QuarterType carry = 0;
+      HalfType carry = 0;
       QuarterType* row = acc;
       for (unsigned j = 0; j < 4; ++j, ++row)
         {
@@ -365,8 +374,11 @@ Uint256::operator *= (const Uint256& x)
           HalfType* sum = (HalfType*) row;
           HalfType prev = *sum;
           *sum += b + HalfType(carry);
-          carry = (*sum < prev)? QuarterType(1) : QuarterType(0);
+          carry = (*sum < prev)? HalfType(1) : HalfType(0);
+          carry <<= sizeof(QuarterType)*8;
         }
+      if (i == 3 and carry > 0)
+        (*row)++;
     }
   *this = prod[0];
   return *this;
@@ -696,7 +708,7 @@ Uint512::operator *= (const Uint512& x)
   for (unsigned i = 0; i < 4; ++i, ++acc)
     {
       QuarterType a = multiplier[i];
-      QuarterType carry = 0;
+      HalfType carry = 0;
       QuarterType* row = acc;
       for (unsigned j = 0; j < 4; ++j, ++row)
         {
@@ -705,8 +717,11 @@ Uint512::operator *= (const Uint512& x)
           HalfType* sum = (HalfType*) row;
           HalfType prev = *sum;
           *sum += b + HalfType(carry);
-          carry = (*sum < prev)? QuarterType(1) : QuarterType(0);
+          carry = (*sum < prev)? HalfType(1) : HalfType(0);
+          carry <<= sizeof(QuarterType)*8;
         }
+      if (i == 3 and carry > 0)
+        (*row)++;
     }
   *this = prod[0];
   return *this;
@@ -1036,7 +1051,7 @@ Uint1024::operator *= (const Uint1024& x)
   for (unsigned i = 0; i < 4; ++i, ++acc)
     {
       QuarterType a = multiplier[i];
-      QuarterType carry = 0;
+      HalfType carry = 0;
       QuarterType* row = acc;
       for (unsigned j = 0; j < 4; ++j, ++row)
         {
@@ -1045,8 +1060,11 @@ Uint1024::operator *= (const Uint1024& x)
           HalfType* sum = (HalfType*) row;
           HalfType prev = *sum;
           *sum += b + HalfType(carry);
-          carry = (*sum < prev)? QuarterType(1) : QuarterType(0);
+          carry = (*sum < prev)? HalfType(1) : HalfType(0);
+          carry <<= sizeof(QuarterType)*8;
         }
+      if (i == 3 and carry > 0)
+        (*row)++;
     }
   *this = prod[0];
   return *this;
@@ -1357,3 +1375,77 @@ Int1024::operator <<= (int n)
     }
   return *this;
 }
+
+
+# if 0
+
+#include <random>
+
+int
+main(int argc, char* argv[])
+{
+  uint64_t a = ~uint64_t(0);
+  __uint128_t c = ~ __uint128_t(0);
+  c *= a;
+
+  Uint128 cc(a, a);
+  cc *= a;
+
+  assert(cc.high() == uint64_t(c >> 64));
+  assert(cc.low() == (c << 64) >> 64);
+
+  c = ~ __uint128_t(0);
+  c *= c;
+
+  cc = Uint128(a, a);
+  cc *= cc;
+
+  if ((cc.high() != c >> 64) or (cc.low() != (c << 64) >> 64))
+    std::cerr << "Failed\n";
+
+  std::mt19937 gen;
+  std::uniform_int_distribution<uint64_t> distrib(0, ~uint64_t(0));
+
+  for (unsigned i = 0; i < 100000000; ++i)
+    {
+      uint64_t low1 = distrib(gen), high1 = distrib(gen);
+      __uint128_t x1 = high1;
+      x1 = (x1 << 64) | low1;
+      Uint128 y1(high1, low1);
+
+      uint64_t low2 = distrib(gen), high2 = distrib(gen);
+      __uint128_t x2 = high2;
+      x2 = (x2 << 64) | low2;
+      Uint128 y2(high2, low2);
+
+      __uint128_t z1 = x1 * x2;
+      Uint128 z2 = y1 * y2;
+
+      if ((z2.high() != z1 >> 64) or (z2.low() != (z1 << 64) >> 64))
+        {
+          std::cerr << std::dec << "Failed " << i << std::hex << " on 0x"
+                    << high1 << " 0x" << low1
+                    << " * 0x" << high2 << " 0x " << low2 << '\n';
+          return 1;
+        }
+
+      __int128_t sx1 = x1, sx2 = x2;
+      __int128_t sz1 = sx1 * sx2;
+
+      Int128 sy1 = y1, sy2 = y2;
+      Int128 sz2 = sy1 * sy2;
+
+      if ((sz2.high() != sz1 >> 64 or z2.low() != (z1 << 64) >> 64))
+        {
+          std::cerr << std::dec << "Failed signed " << i << std::hex << " on 0x"
+                    << high1 << " 0x" << low1
+                    << " * 0x" << high2 << " 0x " << low2 << '\n';
+          return 1;
+        }
+
+    }      
+
+  return 0;
+}
+
+#endif
