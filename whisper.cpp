3a052be6837e7bd7dd28dec7c732ae966d6bc788
Joseph Rahmeh <joseph.rahmeh@wdc.com>
2020-12-02 14:36:49 -0800

    Do not put PIC area loads in load queue (treat them as blocking).

--- whisper.cpp
+++ whisper.cpp
@@ -253,7 +253,7 @@ void
 printVersion()
 {
   unsigned version = 1;
-  unsigned subversion = 601;
+  unsigned subversion = 602;
   std::cout << "Version " << version << "." << subversion << " compiled on "
 	    << __DATE__ << " at " << __TIME__ << '\n';
 }
